2e8148b5c34eb7c4cee61970316b6c2c0776f4f9
kxdzj <2728347961@qq.com>
2025-02-26 21:10:24 +0800

    print pgtbl

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,34 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// Print a page table.
+int 
+pagetableprint(pagetable_t pagetable, int depth) {
+    // there are 2^9 = 512 PTEs in a page table.
+    for(int i = 0; i < 512; i++){
+      pte_t pte = pagetable[i];
+      if(pte & PTE_V){
+        printf("..");
+        // this PTE points to a lower-level page table.
+        for(int j=0; j<depth; j++) {
+          printf(".."); 
+        }
+        printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+        if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+          // this PTE points to a lower-level page table.
+          uint64 child = PTE2PA(pte);
+          pagetableprint((pagetable_t)child, depth+1);
+        }
+      }
+    }
+    return 0;
+}
+
+// Print a page table.
+int
+vmprint(pagetable_t pagetable)
+{ 
+  printf("page table %p\n", pagetable);
+  return pagetableprint(pagetable, 0);
+}
